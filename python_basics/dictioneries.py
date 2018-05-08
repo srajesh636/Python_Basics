b80b7bf28c6f2752462c93f1c7942a4d9c857b1d
Rajesh <srajesh636@gmail.com>
2018-05-08 16:05:06 +0530

    with kets and value

--- python_basics/dictioneries.py
+++ python_basics/dictioneries.py
@@ -1,3 +1,10 @@
 #Dictionaries are nothing by key value pairs
 sample={'name':'sai' ,'gender':'male'}
 print(sample['name'])
+
+
+#get the complete keys
+print(sample.keys())
+
+#get the complete values
+print(sample.values())
