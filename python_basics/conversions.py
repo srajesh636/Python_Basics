0b26d85bf61659c9a8c4f8468c7a9a8ee29c7873
Rajesh <srajesh636@gmail.com>
2018-05-08 16:39:29 +0530

    sets vs/frozensets

---
+++ python_basics/conversions.py
@@ -0,0 +1,17 @@
+#converting number  into  float
+a=20
+print(float(a))
+print(str(a))
+
+#Sets are nothing but the list which does not contains duplicate elements
+#sets are mutable
+
+
+a=[1,2,2,3]
+
+print(set(a))
+
+#frozensets are immutable
+print(frozenset(a))
+
+a[2]=4
